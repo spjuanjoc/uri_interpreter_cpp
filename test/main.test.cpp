2c61aa1b7421336104c43bfe7dafbd42cb2f73f7
Juan Jose Castellanos <juan.castellanos@smartmatic.com>
2020-10-02 16:39:34 -0500

    add unit test main

---
+++ test/main.test.cpp
@@ -0,0 +1,11 @@
+//
+// Created by juan.castellanos on 2/10/20.
+//
+
+#include "gtest/gtest.h"
+
+int main(int argc, char* argv[])
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
