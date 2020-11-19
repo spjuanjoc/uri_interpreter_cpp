b3e677ecc7f8a7dc2dae963002b4d92043dcebbd
Juan Jose Castellanos <juan.castellanos@smartmatic.com>
2020-11-18 21:21:32 -0500

    add conan files to make and test the package

---
+++ .conan/conanfile.py
@@ -0,0 +1,33 @@
+from conans import ConanFile, CMake, tools
+
+class UriInterpreterCppConan(ConanFile):
+    name = "uri_interpreter_cpp"
+    version = "0.0.1"
+    license = "MIT"
+    author = "Juan Jose Castellanos juanchocaste@gmail.com"
+    url = "https://github.com/spjuanjoc/uri_interpreter_cpp"
+    description = "Interpreter to lex and parse URI"
+    topics = ("conan", "URI")
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = {"shared": False}
+    generators = "cmake"
+
+    def source(self):
+        self.run("git clone https://github.com/spjuanjoc/uri_interpreter_cpp")
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="uri_interpreter_cpp")
+        cmake.build()
+
+    def package(self):
+        self.copy("*.h", dst="include", src="uri_interpreter_cpp/include")
+        self.copy("*uri_interpreter_cpp.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.dylib", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["uri_interpreter_cpp"]
---
+++ .conan/test_package/conanfile.py
@@ -0,0 +1,27 @@
+import os
+from conans import ConanFile, CMake, tools
+
+class UriInterpreterCppTestConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+    requires = "uri_interpreter_cpp/0.0.1"
+
+    def build(self):
+        cmake = CMake(self)
+        # Current dir is "test_package/build/<build_id>" and CMakeLists.txt is
+        # in "test_package"
+        cmake.configure()
+        cmake.build()
+
+    def imports(self):
+        self.copy("*.dll", dst="bin", src="bin")
+        self.copy("*.dylib*", dst="bin", src="lib")
+        self.copy('*.so*', dst='bin', src='lib')
+        self.copy('*.a*', dst='bin', src='lib')
+        self.copy('*.a*', dst='', src='lib')
+        self.copy('*.a*', dst='', src='uri_interpreter_cpp')
+
+    def test(self):
+        if not tools.cross_building(self):
+            os.chdir("bin")
+            self.run(".%sexample" % os.sep)
