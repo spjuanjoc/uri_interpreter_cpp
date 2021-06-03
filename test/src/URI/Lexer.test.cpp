09e40249e933c775f51f408591ec7245e9b8fcce
Juan Jose Castellanos <juancho.312@hotmail.com>
2021-06-03 17:21:37 -0500

    feat: add lex for query and fragment
    - Add member lex

--- test/src/URI/Lexer.test.cpp
+++ test/src/URI/Lexer.test.cpp
@@ -1,3 +1,7 @@
+/**
+ * Created by juan.castellanos on 2/06/21.
+*/
+
 #include "URI/Lexer.h"
 
 #include "UriExamples.h"
@@ -20,9 +24,22 @@ TEST_CASE("should create different lexer instances", "[lexer][constructor]")
   CHECK(&lexer1 != &lexer2);
 }
 
+TEST_CASE_METHOD(LexerFixture, "should lex and empty URI", "[empty]")
+{
+  lexer.setUri("");
+  auto result = lexer.lex();
+
+  CHECK(result.scheme.empty());
+  CHECK(result.authority.empty());
+  CHECK(result.path.empty());
+  CHECK(result.query.empty());
+  CHECK(result.fragment.empty());
+}
+
 TEST_CASE_METHOD(LexerFixture, "should lex scheme and path of type mailto", "[scheme][path][mailto]")
 {
-  auto result = lexer.lex(scheme_path_mailto);
+  lexer.setUri(scheme_path_mailto);
+  auto result = lexer.lex();
 
   CHECK(result.scheme == "mailto");
   CHECK(result.path == "John.Doe@example.com");
@@ -30,7 +47,8 @@ TEST_CASE_METHOD(LexerFixture, "should lex scheme and path of type mailto", "[sc
 
 TEST_CASE_METHOD(LexerFixture, "should lex scheme and path of type tel", "[scheme][path][tel]")
 {
-  auto result = lexer.lex(scheme_path_tel);
+  lexer.setUri(scheme_path_tel);
+  auto result = lexer.lex();
 
   CHECK(result.scheme == "tel");
   CHECK(result.path == "+1-816-555-1212");
@@ -38,7 +56,8 @@ TEST_CASE_METHOD(LexerFixture, "should lex scheme and path of type tel", "[schem
 
 TEST_CASE_METHOD(LexerFixture, "should lex scheme and path of type urn", "[scheme][path][urn]")
 {
-  auto result = lexer.lex(scheme_path_urn);
+  lexer.setUri(scheme_path_urn);
+  auto result = lexer.lex();
 
   CHECK(result.scheme == "urn");
   CHECK(result.path == "oasis:names:specification:docbook:dtd:xml:4.1.2");
@@ -46,7 +65,8 @@ TEST_CASE_METHOD(LexerFixture, "should lex scheme and path of type urn", "[schem
 
 TEST_CASE_METHOD(LexerFixture, "should lex scheme and path of type news", "[scheme][path][news]")
 {
-  auto result = lexer.lex(scheme_path_news);
+  lexer.setUri(scheme_path_news);
+  auto result = lexer.lex();
 
   CHECK(result.scheme == "news");
   CHECK(result.path == "comp.infosystems.www.servers.unix");
@@ -54,7 +74,8 @@ TEST_CASE_METHOD(LexerFixture, "should lex scheme and path of type news", "[sche
 
 TEST_CASE_METHOD(LexerFixture, "should lex authority IPv4", "[IPv4]")
 {
-  auto result = lexer.lex(scheme_authorityIPv4);
+  lexer.setUri(scheme_authorityIPv4);
+  auto result = lexer.lex();
 
   CHECK(result.scheme == "telnet");
   CHECK(result.authority == "192.0.2.16:80");
@@ -62,36 +83,63 @@ TEST_CASE_METHOD(LexerFixture, "should lex authority IPv4", "[IPv4]")
 
 TEST_CASE_METHOD(LexerFixture, "should lex authority IPv4 and path", "[IPv4][path]")
 {
-  auto result = lexer.lex(scheme_authorityIPv4_path);
+  lexer.setUri(scheme_authorityIPv4_path);
+  auto result = lexer.lex();
 
   CHECK(result.scheme == "telnet");
   CHECK(result.authority == "192.0.2.17:80");
   CHECK(result.path == "path");
 }
 
-TEST_CASE_METHOD(LexerFixture, "should lex authority IPv6, path, and query", "[IPv6][path][query]")
+TEST_CASE_METHOD(LexerFixture, "should lex authority IPv6, path, and multi query", "[IPv6][path][query]")
 {
-  auto result = lexer.lex(scheme_authorityIPv6_path_query);
+  lexer.setUri(scheme_authorityIPv6_path_query);
+  auto result = lexer.lex();
 
   CHECK(result.scheme == "ldap");
   CHECK(result.authority == "[2001:db8::7]");
-  CHECK(result.path == "c=GB?objectClass?one");
+  CHECK(result.path == "c=GB");
+  CHECK(result.query == "objectClass?one");
 }
 
 TEST_CASE_METHOD(LexerFixture, "should lex path, query, and fragment", "[RN][path][query][fragment]")
 {
-  auto result = lexer.lex(registeredname_path_query_fragment);
+  lexer.setUri(name_path_query_fragment);
+  auto result = lexer.lex();
 
   CHECK(result.scheme == "https");
   CHECK(result.authority == "john.doe@example.com:123");
-  CHECK(result.path == "/forum/questions/?tag=networking&order=newest#top");
+  CHECK(result.path == "/forum/questions/");
+  CHECK(result.query == "tag=networking&order=newest");
+  CHECK(result.fragment == "top");
+}
+
+TEST_CASE_METHOD(LexerFixture, "should lex name, path, and fragment", "[RN][path][fragment]")
+{
+  lexer.setUri(name_path_fragment);
+  auto result = lexer.lex();
+
+  CHECK(result.scheme == "https");
+  CHECK(result.authority == "spjuanjoc.github.io");
+  CHECK(result.path == "/avanzado/fechas-formato-estandar/");
+  CHECK(result.fragment == "usando-la-biblioteca-fmt");
 }
 
 TEST_CASE("should benchmark the lexer", "[lex][benchmark]")
 {
   BENCHMARK("lexer construction and lex")
   {
-    Lexer lexer;
-    lexer.lex(registeredname_path_query_fragment);
+    Lexer lexer{name_path_query_fragment};
+    lexer.lex();
   };
+}
+
+TEST_CASE("should lex as static", "[lex][static]")
+{
+  auto result = Lexer::lex(name_path_fragment);
+
+  CHECK(result.scheme == "https");
+  CHECK(result.authority == "spjuanjoc.github.io");
+  CHECK(result.path == "/avanzado/fechas-formato-estandar/");
+  CHECK(result.fragment == "usando-la-biblioteca-fmt");
 }
