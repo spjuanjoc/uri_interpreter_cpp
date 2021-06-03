e4be8e5b9a2a54deb8f87bf07e5d923527204df9
Juan Jose Castellanos <juancho.312@hotmail.com>
2021-06-02 20:44:11 -0500

    test: add full path test cases

--- test/src/URI/Lexer.test.cpp
+++ test/src/URI/Lexer.test.cpp
@@ -37,3 +37,54 @@ TEST_CASE_METHOD(LexerFixture,"should lex scheme and path of type tel", "[scheme
   CHECK(result.scheme == "tel");
   CHECK(result.path == "+1-816-555-1212");
 }
+
+TEST_CASE_METHOD(LexerFixture,"should lex scheme and path of type urn", "[scheme][path][urn]")
+{
+  auto result = lexer.lex(scheme_path_urn);
+
+  CHECK(result.scheme == "urn");
+  CHECK(result.path == "oasis:names:specification:docbook:dtd:xml:4.1.2");
+}
+
+TEST_CASE_METHOD(LexerFixture, "should lex scheme and path of type news", "[scheme][path][news]")
+{
+  auto result = lexer.lex(scheme_path_news);
+
+  CHECK(result.scheme == "news");
+  CHECK(result.path == "comp.infosystems.www.servers.unix");
+}
+
+TEST_CASE_METHOD(LexerFixture, "should lex authority IPv4", "[IPv4]")
+{
+  auto result = lexer.lex(scheme_authorityIPv4);
+
+  CHECK(result.scheme == "telnet");
+  CHECK(result.authority == "192.0.2.16:80");
+}
+
+TEST_CASE_METHOD(LexerFixture, "should lex authority IPv4 and path", "[IPv4][path]")
+{
+  auto result = lexer.lex(scheme_authorityIPv4_path);
+
+  CHECK(result.scheme == "telnet");
+  CHECK(result.authority == "192.0.2.17:80");
+  CHECK(result.path == "path");
+}
+
+TEST_CASE_METHOD(LexerFixture, "should lex authority IPv6, path, and query", "[IPv6][path][query]")
+{
+  auto result = lexer.lex(scheme_authorityIPv6_path_query);
+
+  CHECK(result.scheme == "ldap");
+  CHECK(result.authority == "[2001:db8::7]");
+  CHECK(result.path == "c=GB?objectClass?one");
+}
+
+TEST_CASE_METHOD(LexerFixture, "should lex path, query, and fragment", "[RN][path][query][fragment]")
+{
+  auto result = lexer.lex(registeredname_path_query_fragment);
+
+  CHECK(result.scheme == "https");
+  CHECK(result.authority == "john.doe@example.com:123");
+  CHECK(result.path == "/forum/questions/?tag=networking&order=newest#top");
+}
