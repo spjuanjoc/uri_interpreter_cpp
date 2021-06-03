5a41f3d380e27b948319f5152af2f9abbe38da95
Juan Jose Castellanos <juancho.312@hotmail.com>
2021-06-02 19:56:26 -0500

    test: use catch2 instead of gtest

--- test/main.test.cpp
+++ test/main.test.cpp
@@ -1,11 +1,2 @@
-//
-// Created by juan.castellanos on 2/10/20.
-//
-
-#include "gtest/gtest.h"
-
-int main(int argc, char* argv[])
-{
-  testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch2/catch.hpp"
