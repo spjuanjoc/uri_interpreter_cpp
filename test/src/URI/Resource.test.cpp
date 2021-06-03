75033fb8867207763c53dc25d3dc97e12b01d703
Juan Jose Castellanos <juancho.312@hotmail.com>
2021-06-02 20:43:42 -0500

    test: migrate cases to catch2

--- test/src/URI/Resource.test.cpp
+++ test/src/URI/Resource.test.cpp
@@ -1,124 +1,97 @@
-//
-// Created by juan.castellanos on 3/10/20.
-//
-
 #include "URI/Resource.h"
+#include "catch2/catch.hpp"
 #include "URI/Parser.h"
 #include "UriExamples.h"
-//#include "uri_examples.h"
-//#include <gtest/gtest.h>
-#include "catch2/catch.hpp"
-#include "fmt/core.h"
 
 using namespace urii;
 
-//class MockIParser : public IParseStrategy
-//{
-//};
-
-//class URIFixture : public testing::Test
-//{
-//};
-
-//
-//TEST_F(URIFixture, lexSchemeAndPathOnly)
-//{
-//  auto tokens = lex(uri2SchemePath);
-//}
-//
-///**
-// * The happy path with IPv4
-// */
-//TEST_F(URIFixture, lexSchemeAuthorityAndPath_IPv4)
-//{
-//  auto tokens = lex(uri3SchemeAuthV4Path2);
-//}
-
-// ---
-
-TEST_CASE("should validate scheme and path ", "[case1]")
+class ResourceFixture
 {
-  Resource uri{scheme_path_mailto};
+public:
+  Resource uri;
+};
+
+TEST_CASE_METHOD(ResourceFixture,"should validate scheme and path ", "[case1]")
+{
+  uri.set(scheme_path_mailto);
 
   CHECK(uri.validate() == true);
   CHECK(uri.get(Component::scheme) == "mailto");
   CHECK(uri.get(Component::path) == "John.Doe@example.com");
 }
 
-TEST_CASE("should validate scheme and path 2", "[case1]")
+TEST_CASE_METHOD(ResourceFixture, "should validate scheme and path 2", "[case1]")
 {
-  Resource uri{scheme_path_tel};
+  uri.set(scheme_path_tel);
 
   CHECK(uri.validate());
   CHECK(uri.get(Component::scheme) == "tel");
   CHECK(uri.get(Component::path) == "+1-816-555-1212");
 }
-//
-//TEST_F(URIFixture, Case1ValidSchemeAndPath3)
-//{
-//  Resource uri{uri2SchemePath3};
-//
-//  EXPECT_TRUE(uri.validate());
-//  EXPECT_STREQ(uri.get(Component::scheme).c_str(), "urn");
-//  EXPECT_STREQ(uri.get(Component::path).c_str(), "oasis:names:specification:docbook:dtd:xml:4.1.2");
-//}
-//
-//TEST_F(URIFixture, Case1ValidSchemeAndPath4)
-//{
-//  Resource uri{uri2SchemePath4};
-//
-//  EXPECT_TRUE(uri.validate());
-//  EXPECT_STREQ(uri.get(Component::scheme).c_str(), "news");
-//  EXPECT_STREQ(uri.get(Component::path).c_str(), "comp.infosystems.www.servers.unix");
-//}
-//
-//TEST_F(URIFixture, Case2ValidSchemeAndAuthorityV4)
-//{
-//  Resource uri{uri3SchemeAuthV4};
-//
-//  EXPECT_TRUE(uri.validate());
-//  EXPECT_STREQ(uri.get(Component::scheme).c_str(), "telnet");
-//  EXPECT_STREQ(uri.get(Component::authority).c_str(), "192.0.2.16:80");
-//}
-//
-//TEST_F(URIFixture, Case3ValidSchemeAuthV4Path)
-//{
-//  Resource uri{uri3SchemeAuthV4Path};
-//
-//  EXPECT_TRUE(uri.validate());
-//  EXPECT_STREQ(uri.get(Component::scheme).c_str(), "telnet");
-//  EXPECT_STREQ(uri.get(Component::authority).c_str(), "192.0.2.16:80");
-//  EXPECT_STREQ(uri.get(Component::path).c_str(), "path");
-//}
-//
-//TEST_F(URIFixture, Case3ValidSchemeAuthV6PathQuery)
-//{
-//  Resource uri{uri4SchemeAuthV6PathQuery};
-//
-//  EXPECT_TRUE(uri.validate());
-//  EXPECT_STREQ(uri.get(Component::scheme).c_str(), "ldap");
-//  EXPECT_STREQ(uri.get(Component::authority).c_str(), "[2001:db8::7]");
-//  EXPECT_STREQ(uri.get(Component::path).c_str(), "c=GB?objectClass?one");
-//}
-//
-//TEST_F(URIFixture, Case4Valid5CompsHostRN)
-//{
-//  Resource uri{uri5SchemeAuthRNPathQueryFrag};
-//
-//  EXPECT_TRUE(uri.validate(Host::RegName));
-//  EXPECT_STREQ(uri.get(Component::scheme).c_str(), "https");
-//  EXPECT_STREQ(uri.get(Component::authority).c_str(), "john.doe@example.com:123");
-//  EXPECT_STREQ(uri.get(Component::path).c_str(), "/forum/questions/?tag=networking&order=newest#top");
-//}
-//
-//TEST_F(URIFixture, LazyCase4Valid5CompsHostRN)
-//{
-//  Resource uri;
-//
-//  uri.set(uri5SchemeAuthRNPathQueryFrag);
-//
-//  EXPECT_TRUE(uri.validate(Host::Unknown));
-//  EXPECT_STREQ(uri.get(Component::scheme).c_str(), "https");
-//  EXPECT_STREQ(uri.get(Component::authority).c_str(), "john.doe@example.com:123");
-//  EXPECT_STREQ(uri.get(Component::path).c_str(), "/forum/questions/?tag=networking&order=newest#top");
-//}
+
+TEST_CASE_METHOD(ResourceFixture, "should validate scheme and path 3", "[case1]")
+{
+  uri.set(scheme_path_urn);
+
+  CHECK(uri.validate());
+  CHECK(uri.get(Component::scheme) == "urn");
+  CHECK(uri.get(Component::path) == "oasis:names:specification:docbook:dtd:xml:4.1.2");
+}
+
+TEST_CASE_METHOD(ResourceFixture, "should validate scheme and path 4", "[case1]")
+{
+  uri.set(scheme_path_news);
+
+  CHECK(uri.validate());
+  CHECK(uri.get(Component::scheme) == "news");
+  CHECK(uri.get(Component::path) == "comp.infosystems.www.servers.unix");
+}
+
+TEST_CASE_METHOD(ResourceFixture, "should validate authority IPv4", "[case2]")
+{
+  uri.set(scheme_authorityIPv4);
+
+  CHECK(uri.validate());
+  CHECK(uri.get(Component::scheme) == "telnet");
+  CHECK(uri.get(Component::authority) == "192.0.2.16:80");
+}
+
+TEST_CASE_METHOD(ResourceFixture, "should validate authority IPv4, and path", "[case3]")
+{
+  uri.set(scheme_authorityIPv4_path);
+
+  CHECK(uri.validate());
+  CHECK(uri.get(Component::scheme) == "telnet");
+  CHECK(uri.get(Component::authority) == "192.0.2.17:80");
+  CHECK(uri.get(Component::path) == "path");
+}
+
+TEST_CASE_METHOD(ResourceFixture, "should validate authority IPv6, path, and query", "[case3]")
+{
+  uri.set(scheme_authorityIPv6_path_query);
+
+  CHECK(uri.validate());
+  CHECK(uri.get(Component::scheme) == "ldap");
+  CHECK(uri.get(Component::authority) == "[2001:db8::7]");
+  CHECK(uri.get(Component::path) == "c=GB?objectClass?one");
+}
+
+TEST_CASE_METHOD(ResourceFixture, "should validate full URI", "[case4]")
+{
+  uri.set(registeredname_path_query_fragment);
+
+  CHECK(uri.validate(Host::RegName));
+  CHECK(uri.get(Component::scheme) == "https");
+  CHECK(uri.get(Component::authority) == "john.doe@example.com:123");
+  CHECK(uri.get(Component::path) == "/forum/questions/?tag=networking&order=newest#top");
+}
+
+TEST_CASE("should validate full URI from constructor", "[case4]")
+{
+  Resource uri{registeredname_path_query_fragment_2};
+
+  CHECK(uri.validate(Host::Unknown));
+  CHECK(uri.get(Component::scheme) == "https");
+  CHECK(uri.get(Component::authority) == "sitechecker.pro:8080");
+  CHECK(uri.get(Component::path) == "/knowledge-base/path?name=article&topic=seo#top");
+}
