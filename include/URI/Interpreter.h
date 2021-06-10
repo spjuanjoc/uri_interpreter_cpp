47d8e33d3bc79a6c9e168fa23c6b65b72e19b70d
Juan Jose Castellanos <juancho.312@hotmail.com>
2021-06-09 22:25:02 -0500

    feat: add empty interface with the implementation plan

---
+++ include/URI/Interpreter.h
@@ -0,0 +1,55 @@
+/**
+ * @brief   The URI interpreter implementation plan.
+ *
+ * @author  juan.castellanos
+ * @date    2021-06-09
+ */
+
+#ifndef URII_INTERPRETER_H
+#define URII_INTERPRETER_H
+
+/**
+URI interpreter plan:
+
+ 1. Components
+  scheme      mandatory
+  path        mandatory
+  authority
+    host
+    userinfo
+    port
+  query
+  fragment
+
+ 2. Interface
+    IResource
+       Validate(host type: v4, v6, RN)
+        - validate mandatory components
+        - validate host sub component
+        - then validate the rest
+       GetComponent(component)
+
+ 3. Implementation
+    Resource
+      validate -> Dynamic Strategy
+          Dynamic host parser
+            Interface IParser
+              IPv6Parser
+              IPv4Parser
+              RNParser
+      get
+
+ 4. Usage
+    lexer
+    parser
+      The dynamic strategy usage class
+
+ 5. resolver? interpreter?
+    what to do with the parsed data
+    auto parsed = parse("URI");
+    parsed->validate(Host::IPv6);
+    parsed->Get(Component::scheme)
+
+ */
+
+#endif /* URII_INTERPRETER_H */
